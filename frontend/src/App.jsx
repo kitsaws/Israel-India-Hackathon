18a503751df3d4a9aafb03ebd01d4299dc0fbce3
kitsaws <nagpalswastik@gmail.com>
2025-08-05 23:42:16 +0530

    Update index.css, main.jsx, app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
+import AppRoutes from './routes/AppRoutes'
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div className='min-h-screen bg-background text-text'>
+      <AppRoutes />
+    </div>
   )
 }
 
