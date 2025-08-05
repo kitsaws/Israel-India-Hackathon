d1ad326232fd0540c36427b0dcc240cbfe4e68e2
kitsaws <nagpalswastik@gmail.com>
2025-08-05 14:31:33 +0530

    Add EyeTrackingToast

---
+++ frontend/src/components/ui/EyeTrackingToast.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EyeTrackingToast = () => {
+  return (
+    <div className='absolute bottom-0 left-0 m-5 px-4 py-2 rounded-full bg-white border-1 border-text-muted text-text-muted'>
+      👁️ Eye tracking active. Blink to select items.
+    </div>
+  )
+}
+
+export default EyeTrackingToast
