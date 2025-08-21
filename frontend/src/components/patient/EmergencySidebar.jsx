8fd4d16be9633b20ec78c31206dba99a221fd0eb
kitsaws <nagpalswastik@gmail.com>
2025-08-21 21:01:26 +0530

    changes to Patient Pages
    Added Call functionality to Call Nurse btn in EmergencySidebar; Fixed Navbar logo size

--- frontend/src/components/patient/EmergencySidebar.jsx
+++ frontend/src/components/patient/EmergencySidebar.jsx
@@ -1,7 +1,17 @@
 import { TriangleAlert, Heart, Phone, Clock } from 'lucide-react'
 import SidebarTiles from '../ui/SidebarTiles'
+import axios from 'axios'
 
 const EmergencySidebar = () => {
+  const handleNurseCall = async () => {
+    try {
+      await axios.post('http://localhost:3000/api/call/connect', {}, { withCredentials: true })
+      console.log('Calling Nurse...')
+    } catch (err) {
+      console.error('Error calling the nurse: ', err);
+    }
+  };
+
   return (
     <div className='emergency-sidebar bg-secondary w-[20%] py-8 px-4'>
       <div id="logo" className='flex flex-col justify-center items-center text-2xl font-bold text-secondary-text'>
@@ -9,7 +19,9 @@ const EmergencySidebar = () => {
         <span>Emergency Services</span>
       </div>
       <div className='flex flex-col gap-6 mt-6'>
-        <SidebarTiles logo={<Phone />} title={'Call Nurse'} />
+        <button onClick={handleNurseCall}>
+          <SidebarTiles logo={<Phone />} title={'Call Nurse'} />
+        </button>
         <SidebarTiles logo={<TriangleAlert />} title={'Pain'} />
         <SidebarTiles logo={<Heart />} title={'Breathing'} />
         <SidebarTiles logo={<Clock />} title={'Urgent'} />
